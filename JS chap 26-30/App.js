31c6ebb855826a5da06fd25be27bc83a8bf772a4
Mustafa Faisal <122450274+MustafaFaisal-778@users.noreply.github.com>
2023-03-29 00:34:00 +0500

    Add files via upload

---
+++ JS chap 26-30/App.js
@@ -0,0 +1,71 @@
+// // Question#1
+
+// var input=+prompt("enter a positive number")
+// console.log(Math.round(input))
+// console.log(Math.floor(input))
+// console.log(Math.ceil(input))
+
+// // Question#2
+
+// var input=+prompt("enter a Negative number")
+// console.log(Math.round(input))
+// console.log(Math.floor(input))
+// console.log(Math.ceil(input))
+
+// Question#4
+// var input=Math.random()*6
+// console.log(Math.round(input))
+// 
+// Question#6
+// var input=Math.random()*100
+// console.log(Math.round(input))
+
+// Question#7
+// var secretNumber=Math.round(Math.random()*100)
+// console.log(secretNumber)
+// for(i=1;i<=10;i++){
+// console.log(i)
+// var input=+prompt("Enter a number")
+// if(input<secretNumber){
+//     console.log("the number is less than secret number")
+// }
+// else if(input>secretNumber){
+//     console.log("the number is  than greater secret number")
+// }
+// else if(input=secretNumber){
+//     console.log("You Win")
+//     break
+// }
+// }
+
+// Question#8
+// var player1=prompt("enter 1 player name")
+// var player2=prompt("enter 2nd player name")
+// var headTail=prompt("Head or Tail")
+// var head=0
+// var tail=0
+// var random= Math.random()*1
+// var round= Math.round(random)
+// console.log(round)
+// if(round==1){
+//     var head="head"
+//     if(headTail=="head"){   
+//         alert("you win")
+
+//     }
+//     else{
+//         alert("you lose")
+//     }
+// }
+// else if(round==0){
+//     var tail="tail"
+//     if(headTail=="tail"){   
+//         alert("you win")
+
+//     }
+//     else{
+//         alert("you lose")
+//     }
+// }
+
+
